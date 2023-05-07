0710b03321cdb501a1165333531a9bf475f88a9f
bitRite <glf123456789@mail.ru>
2023-05-07 19:17:44 +0300

    Добавление формы входа

--- Messanger Project/Messanger Project/Messanger.Designer.cs
+++ Messanger Project/Messanger Project/Messanger.Designer.cs
@@ -28,21 +28,162 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.мессенджерToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.выйтиИзАккаунтаToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.закрытьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.linkRegister = new System.Windows.Forms.LinkLabel();
+            this.labPhone = new System.Windows.Forms.Label();
+            this.txtbPhone = new System.Windows.Forms.TextBox();
+            this.txtBoxPassword = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.chkRemember = new System.Windows.Forms.CheckBox();
+            this.butSignIn = new System.Windows.Forms.Button();
+            this.menuStrip1.SuspendLayout();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
-            // Form1
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.мессенджерToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(323, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // мессенджерToolStripMenuItem
+            // 
+            this.мессенджерToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.выйтиИзАккаунтаToolStripMenuItem,
+            this.закрытьToolStripMenuItem});
+            this.мессенджерToolStripMenuItem.Name = "мессенджерToolStripMenuItem";
+            this.мессенджерToolStripMenuItem.Size = new System.Drawing.Size(89, 20);
+            this.мессенджерToolStripMenuItem.Text = "Мессенджер";
+            // 
+            // выйтиИзАккаунтаToolStripMenuItem
+            // 
+            this.выйтиИзАккаунтаToolStripMenuItem.Name = "выйтиИзАккаунтаToolStripMenuItem";
+            this.выйтиИзАккаунтаToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.выйтиИзАккаунтаToolStripMenuItem.Text = "Выйти из аккаунта";
+            // 
+            // закрытьToolStripMenuItem
+            // 
+            this.закрытьToolStripMenuItem.Name = "закрытьToolStripMenuItem";
+            this.закрытьToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.закрытьToolStripMenuItem.Text = "Закрыть";
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.butSignIn);
+            this.panel1.Controls.Add(this.chkRemember);
+            this.panel1.Controls.Add(this.txtBoxPassword);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.txtbPhone);
+            this.panel1.Controls.Add(this.labPhone);
+            this.panel1.Controls.Add(this.linkRegister);
+            this.panel1.Location = new System.Drawing.Point(12, 27);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(299, 445);
+            this.panel1.TabIndex = 1;
+            // 
+            // linkRegister
+            // 
+            this.linkRegister.AutoSize = true;
+            this.linkRegister.Location = new System.Drawing.Point(106, 368);
+            this.linkRegister.Name = "linkRegister";
+            this.linkRegister.Size = new System.Drawing.Size(79, 13);
+            this.linkRegister.TabIndex = 0;
+            this.linkRegister.TabStop = true;
+            this.linkRegister.Text = "Новый аккант";
+            // 
+            // labPhone
+            // 
+            this.labPhone.AutoSize = true;
+            this.labPhone.Location = new System.Drawing.Point(62, 120);
+            this.labPhone.Name = "labPhone";
+            this.labPhone.Size = new System.Drawing.Size(96, 13);
+            this.labPhone.TabIndex = 1;
+            this.labPhone.Text = "Номер телефона:";
+            // 
+            // txtbPhone
+            // 
+            this.txtbPhone.Location = new System.Drawing.Point(65, 137);
+            this.txtbPhone.Name = "txtbPhone";
+            this.txtbPhone.Size = new System.Drawing.Size(152, 20);
+            this.txtbPhone.TabIndex = 2;
+            // 
+            // txtBoxPassword
+            // 
+            this.txtBoxPassword.Location = new System.Drawing.Point(65, 182);
+            this.txtBoxPassword.Name = "txtBoxPassword";
+            this.txtBoxPassword.Size = new System.Drawing.Size(152, 20);
+            this.txtBoxPassword.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(62, 165);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(48, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Пароль:";
+            // 
+            // chkRemember
+            // 
+            this.chkRemember.AutoSize = true;
+            this.chkRemember.Location = new System.Drawing.Point(65, 209);
+            this.chkRemember.Name = "chkRemember";
+            this.chkRemember.Size = new System.Drawing.Size(111, 17);
+            this.chkRemember.TabIndex = 5;
+            this.chkRemember.Text = "Запомнить меня";
+            this.chkRemember.UseVisualStyleBackColor = true;
+            // 
+            // butSignIn
+            // 
+            this.butSignIn.Location = new System.Drawing.Point(88, 253);
+            this.butSignIn.Name = "butSignIn";
+            this.butSignIn.Size = new System.Drawing.Size(111, 35);
+            this.butSignIn.TabIndex = 6;
+            this.butSignIn.Text = "Войти";
+            this.butSignIn.UseVisualStyleBackColor = true;
+            // 
+            // Messanger
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Name = "Form1";
+            this.ClientSize = new System.Drawing.Size(323, 484);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Messanger";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem мессенджерToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem выйтиИзАккаунтаToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem закрытьToolStripMenuItem;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button butSignIn;
+        private System.Windows.Forms.CheckBox chkRemember;
+        private System.Windows.Forms.TextBox txtBoxPassword;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtbPhone;
+        private System.Windows.Forms.Label labPhone;
+        private System.Windows.Forms.LinkLabel linkRegister;
     }
 }
 
